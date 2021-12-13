07ff6e8409e504ffd520c53c0698f70007645c23
ra-ran <ucreo@naver.com>
2021-12-13 15:14:47 +0900

    3-9 completed

--- pybo/models.py
+++ pybo/models.py
@@ -7,6 +7,7 @@ class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
+    modify_date = models.DateTimeField(null=True, blank=True)
     create_date = models.DateTimeField()
 
     def __str__(self):
@@ -16,5 +17,6 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
+    modify_date = models.DateTimeField(null=True, blank=True)
     create_date = models.DateTimeField()
 
